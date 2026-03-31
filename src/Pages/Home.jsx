aa2b1839de0dd432ceaee25fd0983f82161479d9
Natty24 <malifeaye2485@gmail.com>
2026-04-01 01:40:29 +0300

    Fix: Completed truncated code in Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -88,4 +88,12 @@ const Home = () => {
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
           {filteredListings.map((item) => (
-            <ListingCard key={item.
+            <ListingCard key={item.id} item={item} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;      
