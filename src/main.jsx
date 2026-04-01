dd62b4666c7213c0040a8585776bb2afc4bb2722
Natty24 <malifeaye2485@gmail.com>
2026-04-01 03:55:47 +0300

    Fix: Vite base path and confirmed global style imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import './index.css';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css' // CRITICAL: This links your black background!
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
