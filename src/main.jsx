c03fd87004c9d2b86d1ac7782de127d8c7fdca8f
Natty24 <malifeaye2485@gmail.com>
2026-04-02 01:23:30 +0300

    Fix: Ensure Tailwind CSS is properly imported

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // CRITICAL: This links your black background!
+import './index.css' // ይህ መስመር መኖሩ በጣም ወሳኝ ነው!
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
